327f0aa1536af2a3369c792b28ae302472b59c0b
SeyeongPark <lynn.yeong.102@gmail.com>
2022-04-25 17:00:23 -0400

    trying fix deploy error

--- config/express.js
+++ config/express.js
@@ -7,11 +7,9 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 
 module.exports = function() {
-    const PORT = process.env.PORT || '5000'
 
 	// Create a new Express application instance
 	const app = express();
-	app.set("port",PORT);
 
 	// Use the 'NDOE_ENV' variable to activate the 'morgan' logger or 'compress' middleware
 	if (process.env.NODE_ENV === 'development') {
@@ -19,6 +17,10 @@ module.exports = function() {
 	} else if (process.env.NODE_ENV === 'production') {
 		app.use(compress());
 	}
+
+	app.listen(process.env.PORT || 3000, function(){
+		console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+	  });
 
 
 	// Use the 'body-parser' and 'method-override' middleware functions
